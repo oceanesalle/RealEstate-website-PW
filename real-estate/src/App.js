2c699d7dbed44914244ce5447cc6a8e822e7f4e5
oceansalle <oceanesalleiae@gmail.com>
2022-06-20 13:50:03 +0200

    solving error

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <Navbar />
+      </div>
   );
 }
 
--- real-estate/src/components/Navbar.js
+++ real-estate/src/components/Navbar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Nav = styled.nav`
+  height: 64px;
+  background: red;
+`; 
+
+const Logo = styled(Link)`
+color: #fff;
+`; 
+
+const MenuBars = styled.i``; 
+const NavMenu = styled.i``;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav>
+      <Logo>WEBSITE</Logo>
+      <MenuBars/>
+      <NavMenu />
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
